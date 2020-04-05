5cc6d19d05bb30cee74ce29908ab6d29cf6ea014
mr_sobol <mr_sobol@ukr.net>
2020-04-05 12:44:27 +0300

    Final version lesson 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "lib.h"
+
 
 int main()
 {
-   std::cout<<"Hello world"<<std::endl;
+   std::cout<<"build "<<version()<<std::endl;
+   std::cout<<"Hello, World!"<<std::endl;
 }
